96113843f3b8bbd66d1ed0571abf592f9a81f21b
yaolili <yaolili12235@gmail.com>
2015-12-17 11:40:05 +0800

    fix one mistake

--- code/runB.sh
+++ code/runB.sh
@@ -7,24 +7,24 @@
 
 echo "=================dev classification==================="
 
-python runClassification.py gbdt data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/orderorderYNDevGBDT.txt
+python runClassification.py gbdt data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/order/orderYNDevGBDT.txt
 
-python runClassification.py essemble data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/orderorderYNDevESSEMBLE.txt
+python runClassification.py essemble data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/order/orderYNDevESSEMBLE.txt
 
-python runClassification.py tree data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/orderorderYNDevTREE.txt
+python runClassification.py tree data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/order/orderYNDevTREE.txt
 
-python runClassification.py knn data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/orderorderYNDevKNN.txt
+python runClassification.py knn data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/order/orderYNDevKNN.txt
 
-python runClassification.py svm data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/orderorderYNDevSVM.txt
+python runClassification.py svm data/yn/formatResult/finalFormat/baseQAllFeature_train.txt data/yn/formatResult/finalFormat/baseQAllFeature_dev.txt data/yn/order/orderYNDevSVM.txt
 
 echo "=================dev prediction result==================="
 
-python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/orderorderYNDevGBDT.txt data/yn/devPredict/devGBDTYN.txt
+python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/order/orderYNDevGBDT.txt data/yn/devPredict/devGBDTYN.txt
 
-python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/orderorderYNDevESSEMBLE.txt data/yn/devPredict/devESSEMBLEYN.txt
+python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/order/orderYNDevESSEMBLE.txt data/yn/devPredict/devESSEMBLEYN.txt
 
-python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/orderorderYNDevTREE.txt data/yn/devPredict/devTREEYN.txt
+python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/order/orderYNDevTREE.txt data/yn/devPredict/devTREEYN.txt
 
-python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/orderorderYNDevKNN.txt data/yn/devPredict/devKNNYN.txt
+python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/order/orderYNDevKNN.txt data/yn/devPredict/devKNNYN.txt
 
-python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/orderorderYNDevSVM.txt data/yn/devPredict/devSVMYN.txt
+python dealResultYN.py data/yn/prefix/prefixDevYN.txt data/yn/order/orderYNDevSVM.txt data/yn/devPredict/devSVMYN.txt
