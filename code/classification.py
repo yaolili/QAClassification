047b6bf716210636e3947413d328587abb6c6ac6
yaolili <yaolili12235@gmail.com>
2015-12-20 12:11:10 +0800

    ok with grid_search

--- code/classification.py
+++ code/classification.py
@@ -17,6 +17,9 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import  accuracy_score  
 from sklearn.metrics import confusion_matrix    
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.naive_bayes import MultinomialNB 
+from sklearn.preprocessing import MinMaxScaler
 
 #notice, we only use gbdt, essemble, tree, knn and svm classifier
 #you can add others if you need
@@ -34,6 +37,10 @@ class Classification():
         pre = np.loadtxt(f)
         X_test = pre[:, 1:]
         
+        # min_max_scaler = MinMaxScaler()
+        # X_train = min_max_scaler.fit_transform(X_train)
+        # X_test = min_max_scaler.fit_transform(X_test)
+        
         #fit transform if there exit NAN or INFINITE
         #otherwise you'll get error when clf.predict()
         X_test = Imputer().fit_transform(X_test) 
@@ -45,26 +52,50 @@ class Classification():
         f.close()
         
         if classifier == 'tree':
-            clf = tree.DecisionTreeClassifier()
-            #print clf
+            #max_depth = 4 is best
+            # max_depth = np.arange(1, 10)
+            # clf = GridSearchCV(tree.DecisionTreeClassifier(), param_grid = {'max_depth': max_depth})
+            clf = tree.DecisionTreeClassifier(max_depth = 4)
+            
         elif classifier == 'knn':
-            clf = KNeighborsClassifier()
-            #print clf
+            #n_neighbors = 9 is best
+            #but default is 5, better than 9?
+            # n_neighbors = np.arange(1, 10)  
+            # clf = GridSearchCV(KNeighborsClassifier(), param_grid = {'n_neighbors': n_neighbors})
+            clf = KNeighborsClassifier(n_neighbors = 9)
+            
         elif classifier == 'svm':
-            clf = svm.SVC(kernel='rbf')
-            #print clf
+            #{'kernel': 'rbf', 'C': 100, 'gamma': 0.001}
+            # param_grid = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']}, {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']}]
+            # clf = GridSearchCV(svm.SVC(), param_grid)
+            clf = svm.SVC(kernel='rbf', gamma = 0.001, C = 100)
+            
         elif classifier == 'gbdt':
-            clf = GradientBoostingClassifier()
-            #print clf
+            # max_depth = np.arange(1, 10)
+            # n_estimators = [10, 100, 1000]
+            # learning_rate = [0.1, 0.2, 0.3, 0.4, 0.5]
+            # clf = GridSearchCV(GradientBoostingClassifier(), param_grid = {'max_depth': max_depth, 'n_estimators': n_estimators, 'learning_rate': learning_rate})
+            clf = GradientBoostingClassifier(n_estimators = 1000, max_depth = 10)
+            
         elif classifier == 'essemble':
-            clf = RandomForestClassifier(n_estimators=10)
-            #print clf
+            #{'n_estimators': 10, 'max_depth': 6}
+            # max_depth = np.arange(1, 10)
+            # n_estimators = [10, 100, 1000]
+            # clf = GridSearchCV(RandomForestClassifier(), param_grid = {'max_depth': max_depth, 'n_estimators': n_estimators})
+            clf = RandomForestClassifier(n_estimators = 1000, random_state=15325)
+            
+        elif classifier == 'nb':
+            clf = MultinomialNB()
+            print clf
         else:
             print "Invalid classifier in Class Classification __init__()!"
             exit()
         
         clf.fit(X_train, y_train) 
+        #print clf.best_params_
+        
         self.y_pred = clf.predict(X_test)
+        
         '''
         #test usage!
         self.calculate_result(self.y_test, self.y_pred)
